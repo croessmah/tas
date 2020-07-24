3d90d1ea7cebdb25a39982d939e7d6b76972e1cf
croessmah <croessmah@ya.ru>
2020-07-24 13:35:45 +0400

    Добавлен noexcept

--- tas/app_console.cpp
+++ tas/app_console.cpp
@@ -1,6 +1,6 @@
 #include "app_console.h"
 
-tas_app_console::tas_app_console(unsigned _tick_time) :
+tas_app_console::tas_app_console(unsigned _tick_time) noexcept :
     m_controllers{
         "192.168.0.112",
         "192.168.0.111"
@@ -11,7 +11,7 @@ tas_app_console::tas_app_console(unsigned _tick_time) :
 {
 }
 
-int tas_app_console::run()
+int tas_app_console::run() noexcept
 {
     return m_app.run(m_sniffer, m_ipc) ? 
         EXIT_SUCCESS : 
